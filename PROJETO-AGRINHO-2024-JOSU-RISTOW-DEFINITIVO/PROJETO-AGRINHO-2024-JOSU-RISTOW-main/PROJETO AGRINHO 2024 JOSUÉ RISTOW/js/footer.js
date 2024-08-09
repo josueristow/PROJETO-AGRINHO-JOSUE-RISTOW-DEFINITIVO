39f3090a9cf927c3f8feb397aba5596d15d53671
josueristow <126601767+josueristow@users.noreply.github.com>
2024-08-09 18:09:27 -0300

    Add files via upload

---
+++ PROJETO-AGRINHO-2024-JOSU-RISTOW-DEFINITIVO/PROJETO-AGRINHO-2024-JOSU-RISTOW-main/PROJETO AGRINHO 2024 JOSUÉ RISTOW/js/footer.js
@@ -0,0 +1,13 @@
+
+
+instaIcon.addEventListener("click", () => {
+    window.open("https://www.instagram.com", "_blank");
+});
+
+fbIcon.addEventListener("click", () => {
+    window.open("https://www.facebook.com", "_blank");
+});
+
+twitterIcon.addEventListener("click", () => {
+    window.open("https://www.twitter.com", "_blank");
+});
---
+++ PROJETO-AGRINHO-2024-JOSU-RISTOW-DEFINITIVO/PROJETO-AGRINHO-2024-JOSU-RISTOW-main/PROJETO AGRINHO 2024 JOSUÉ RISTOW/js/index.js
@@ -0,0 +1,13 @@
+
+
+botaoAgricultura.addEventListener('click', ()=>{
+    window.location.href = "./paginas/agro-sustentavel.html";
+})
+
+botaoIA.addEventListener('click', ()=>{
+    window.location.href = "./paginas/IA-generativas.html";
+})
+
+botaoCultivo.addEventListener('click', ()=>{
+    window.location.href = "./paginas/cultivos-eficientes.html";
+})
